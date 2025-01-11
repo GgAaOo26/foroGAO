a96e3b0b124f030a9d6f43b998f9c0fd806fdca0
Germán Alejandro Ortiz <gerortiz09@gmail.com>
2025-01-10 21:14:03 -0300

    Initial commit

---
+++ src/main/java/com/foro/foroGAO/ForoGaoApplication.java
@@ -0,0 +1,13 @@
+package com.foro.foroGAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoGaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoGaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foro/foroGAO/ForoGaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foro.foroGAO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoGaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
